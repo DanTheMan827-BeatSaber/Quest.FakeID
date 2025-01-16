4f9fa407c426263155bf994b6cb45db8d9c34924
Daniel <790119+DanTheMan827@users.noreply.github.com>
2025-01-16 15:26:15 -0600

    Add common-stuff submodule and update project configuration
    - Introduced a new submodule for common-stuff.
    - Updated .gitignore to include Hooking.hpp.
    - Removed obsolete modInfo.hpp and Logger.hpp files.
    - Adjusted mod.template.json to use lateModFiles.
    - Updated qpm.json to include hooking script.
    - Modified build.ps1 to include hooking script execution.
    - Added PlayerData hook implementation in src/Hooks/PlayerData.cpp.

--- scripts/build.ps1
+++ scripts/build.ps1
@@ -6,6 +6,11 @@ Param(
     [Switch] $help
 )
 
+# Run the hooking script to generate the hooking code
+Push-Location "$PSScriptRoot/.."
+& qpm s make-hooking
+Pop-Location
+
 if ($help -eq $true) {
     Write-Output "`"Build`" - Copiles your mod into a `".so`" or a `".a`" library"
     Write-Output "`n-- Arguments --`n"
@@ -18,14 +23,14 @@ if ($help -eq $true) {
 # if user specified clean, remove all build files
 if ($clean.IsPresent) {
     if (Test-Path -Path "build") {
-        remove-item build -R -Force 
+        remove-item build -R -Force
     }
 }
 
 
 if (($clean.IsPresent) -or (-not (Test-Path -Path "build"))) {
     new-item -Path build -ItemType Directory
-} 
+}
 
 & cmake -G "Ninja" -DCMAKE_BUILD_TYPE="RelWithDebInfo" -B build
 & cmake --build ./build
--- src/main.cpp
+++ src/main.cpp
@@ -1,85 +1,8 @@
-#include "main.hpp"
 #include "Logger.hpp"
+#include "beatsaber-hook/shared/utils/il2cpp-functions.hpp"
+#include "main.hpp"
 #include "modInfo.hpp"
-
-#include "beatsaber-hook/shared/utils/hooking.hpp"
-#include "GlobalNamespace/BeatmapCharacteristicSO.hpp"
-#include "GlobalNamespace/ColorSchemesSettings.hpp"
-#include "GlobalNamespace/GameplayModifiers.hpp"
-#include "GlobalNamespace/MultiplayerModeSettings.hpp"
-#include "GlobalNamespace/OverrideEnvironmentSettings.hpp"
-#include "GlobalNamespace/PlayerAgreements.hpp"
-#include "GlobalNamespace/PlayerAllOverallStatsData.hpp"
-#include "GlobalNamespace/PlayerData.hpp"
-#include "GlobalNamespace/PlayerLevelStatsData.hpp"
-#include "GlobalNamespace/PlayerMissionStatsData.hpp"
-#include "GlobalNamespace/PlayerSpecificSettings.hpp"
-#include "GlobalNamespace/PracticeSettings.hpp"
-#include "System/Collections/Generic/List_1.hpp"
-
-MAKE_HOOK_FIND_INSTANCE(PlayerData_ctor, classof(GlobalNamespace::PlayerData*), ".ctor", void, GlobalNamespace::PlayerData* self,
-    StringW playerId,
-    StringW playerName,
-    bool shouldShowTutorialPrompt,
-    bool shouldShow360Warning,
-    bool agreedToEula,
-    bool didSelectLanguage,
-    bool agreedToMultiplayerDisclaimer,
-    int32_t didSelectRegionVersion,
-    StringW selectedAvatarSystemTypeId,
-    GlobalNamespace::PlayerAgreements* playerAgreements,
-    GlobalNamespace::BeatmapDifficulty lastSelectedBeatmapDifficulty,
-    GlobalNamespace::BeatmapCharacteristicSO* lastSelectedBeatmapCharacteristic,
-    GlobalNamespace::GameplayModifiers* gameplayModifiers,
-    GlobalNamespace::PlayerSpecificSettings* playerSpecificSettings,
-    GlobalNamespace::PracticeSettings* practiceSettings,
-    GlobalNamespace::PlayerAllOverallStatsData* playerAllOverallStatsData,
-    System::Collections::Generic::List_1<GlobalNamespace::PlayerLevelStatsData*>* levelsStatsData,
-    System::Collections::Generic::List_1<GlobalNamespace::PlayerMissionStatsData*>* missionsStatsData,
-    System::Collections::Generic::List_1<::StringW>* showedMissionHelpIds,
-    System::Collections::Generic::List_1<::StringW>* guestPlayerNames,
-    GlobalNamespace::ColorSchemesSettings* colorSchemesSettings,
-    GlobalNamespace::OverrideEnvironmentSettings* overrideEnvironmentSettings,
-    System::Collections::Generic::List_1<::StringW>* favoritesLevelIds,
-    GlobalNamespace::MultiplayerModeSettings* multiplayerModeSettings,
-    int32_t currentDlcPromoDisplayCount,
-    StringW currentDlcPromoId,
-    GlobalNamespace::UserAgeCategory userAgeCategory,
-    GlobalNamespace::PlayerSensitivityFlag desiredSensitivityFlag) {
-        // We're past the hook definition, call it with the age set to adult.
-        PlayerData_ctor(
-            self,
-            playerId,
-            playerName,
-            shouldShowTutorialPrompt,
-            shouldShow360Warning,
-            agreedToEula,
-            didSelectLanguage,
-            agreedToMultiplayerDisclaimer,
-            didSelectRegionVersion,
-            selectedAvatarSystemTypeId,
-            playerAgreements,
-            lastSelectedBeatmapDifficulty,
-            lastSelectedBeatmapCharacteristic,
-            gameplayModifiers,
-            playerSpecificSettings,
-            practiceSettings,
-            playerAllOverallStatsData,
-            levelsStatsData,
-            missionsStatsData,
-            showedMissionHelpIds,
-            guestPlayerNames,
-            colorSchemesSettings,
-            overrideEnvironmentSettings,
-            favoritesLevelIds,
-            multiplayerModeSettings,
-            currentDlcPromoDisplayCount,
-            currentDlcPromoId,
-            GlobalNamespace::UserAgeCategory::Adult,
-            desiredSensitivityFlag
-        );
-};
-
+#include "scotland2/shared/modloader.h"
 
 /// @brief Called at the early stages of game loading
 /// @param info
@@ -94,8 +17,4 @@ MOD_EXPORT_FUNC void setup(CModInfo& info) {
 /// @return
 MOD_EXPORT_FUNC void late_load() {
     il2cpp_functions::Init();
-
-    Logger.info("Installing hooks...");
-    INSTALL_HOOK(Logger, PlayerData_ctor);
-    Logger.info("Installed all hooks!");
 }

bce2c35c1ecf62fb9279c4a03f23fedb9c96455e
Daniel <790119+DanTheMan827@users.noreply.github.com>
2025-01-23 14:41:16 -0600

    Refactor to use PlayerDataFileModel_Load as the hook
    This results in the hook running when the PlayerData has been both created or loaded from the filesystem

---
+++ src/Hooks/PlayerDataFileModel.cpp
@@ -0,0 +1,18 @@
+#include "Hooking.hpp"
+#include "GlobalNamespace/PlayerData.hpp"
+#include "GlobalNamespace/PlayerDataModel.hpp"
+#include "GlobalNamespace/PlayerDataFileModel.hpp"
+#include "GlobalNamespace/PlayerSensitivityFlag.hpp"
+#include "GlobalNamespace/PlayerAgreements.hpp"
+
+MAKE_DLOPEN_HOOK_MATCH(PlayerDataFileModel_Load, &GlobalNamespace::PlayerDataFileModel::LoadOrCreateFromJsonString, GlobalNamespace::PlayerData*, GlobalNamespace::PlayerDataFileModel* self, StringW jsonString) {
+    auto value = PlayerDataFileModel_Load(self, jsonString);
+
+    if (value->userAgeCategory != GlobalNamespace::UserAgeCategory::Adult) {
+        value->userAgeCategory = GlobalNamespace::UserAgeCategory::Adult;
+        value->desiredSensitivityFlag = GlobalNamespace::PlayerSensitivityFlag::Explicit;
+        value->playerAgreements->playerSensitivityFlagVersion = 3;
+    }
+
+    return value;
+}

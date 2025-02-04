dbdbd9875bf90e963f632481f32db50d981fd2f3
Daniel <790119+DanTheMan827@users.noreply.github.com>
2025-02-04 00:18:43 -0600

    Update template

--- src/Hooks/PlayerDataFileModel.cpp
+++ src/Hooks/PlayerDataFileModel.cpp
@@ -5,7 +5,7 @@
 #include "GlobalNamespace/PlayerSensitivityFlag.hpp"
 #include "GlobalNamespace/PlayerAgreements.hpp"
 
-MAKE_DLOPEN_HOOK_MATCH(PlayerDataFileModel_Load, &GlobalNamespace::PlayerDataFileModel::LoadOrCreateFromJsonString, GlobalNamespace::PlayerData*, GlobalNamespace::PlayerDataFileModel* self, StringW jsonString) {
+MAKE_LATE_HOOK_MATCH(PlayerDataFileModel_Load, &GlobalNamespace::PlayerDataFileModel::LoadOrCreateFromJsonString, GlobalNamespace::PlayerData*, GlobalNamespace::PlayerDataFileModel* self, StringW jsonString) {
     auto value = PlayerDataFileModel_Load(self, jsonString);
 
     if (value->userAgeCategory != GlobalNamespace::UserAgeCategory::Adult) {
